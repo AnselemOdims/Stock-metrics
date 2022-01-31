1e665ee1e72fdfc5216fb47f7b15460bfc639c1d
Anselem <anselemodims@gmail.com>
2022-01-31 14:40:33 +0100

    Feat: build out the Card component

---
+++ src/components/Card.js
@@ -0,0 +1,82 @@
+import styled from 'styled-components';
+import { BsArrowUp, BsArrowDown, BsArrowRightCircle } from 'react-icons/bs';
+import { NavLink } from 'react-router-dom';
+import { motion } from 'framer-motion';
+import Img from '../images/stock_card.svg';
+
+const Container = styled.div`
+	border: solid 1px #F5EEDC;
+	background-color: #E8E8A6;
+	padding: 5px 5px 10px 12px;
+	border-radius: 5px;
+  background-image: url(${Img});
+  background-repeat: no-repeat;
+  background-position: 100% 245%;
+  background-size: contain;
+
+	> a {
+		text-decoration: none;
+    color: #133B5C;
+
+		> div:nth-of-type(1) {
+			display: flex;
+			justify-content: space-between;
+			align-items: center;
+			margin-bottom: 5px;
+
+			> h2 {
+				margin: 0;
+			}
+		}
+
+    > span {
+      background: #f7fad3;
+      border-radius: 8px;
+      padding: 3px 10px;
+      font-size: 10px;
+    }
+
+		> div:nth-of-type(2) {
+			display: flex;
+			flex-direction: column;
+			margin-top: 10px;
+      font-size: 14px;
+
+       span:first-of-type {
+        margin-bottom: 10px;
+        font-weight: bold;
+       }
+		}
+	}
+`;
+const Card = ({ id }) => {
+	return (
+		<motion.div
+			whileHover={{ scale: 1.01, borderRadius: 10 }}
+			transition={{ duration: 1 }}
+		>
+			<Container>
+				<NavLink to={`/details/${id}`}>
+					<div>
+						<h2>Apple Inc.</h2>
+						<BsArrowRightCircle />
+					</div>
+					<span>AAPL</span>
+					<div>
+						<span>$20,000</span>
+						<span>
+							<BsArrowUp color='green' />
+							+2.03%
+						</span>
+						<span>
+							<BsArrowDown color='red' />
+							-0.50%
+						</span>
+					</div>
+				</NavLink>
+			</Container>
+		</motion.div>
+	);
+};
+
+export default Card;
