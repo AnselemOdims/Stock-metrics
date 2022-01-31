34ad5ad7bca931ff9cd35cb863b0cb643c66e405
Anselem <anselemodims@gmail.com>
2022-01-31 14:14:43 +0100

    Feat: setup the App.js with routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
-function App() {
+import {Routes, Route, useLocation} from 'react-router-dom';
+import { AnimatePresence } from 'framer-motion';
+import Details from './components/pages/Details';
+import Home from './components/pages/Home';
+
+const App = () => {
+  const location = useLocation();
   return (
-    <div className="App" />
+    <div className="App">
+        <AnimatePresence>
+          <Routes location={location} key={location.key}>
+            <Route path='/' element={<Home />} />
+            <Route path='/details/:id' element={<Details />} />
+          </Routes>
+        </AnimatePresence>
+    </div>
   );
 }
 
