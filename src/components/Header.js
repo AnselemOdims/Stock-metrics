21679aef67be3d3cbf193d22478c35e6fb2b40cf
Anselem <anselemodims@gmail.com>
2022-01-31 14:09:11 +0100

    Feat: build out and style the Header component

---
+++ src/components/Header.js
@@ -0,0 +1,57 @@
+import styled from "styled-components";
+import { BsFillMicFill } from 'react-icons/bs';
+import { IoIosSettings, IoIosArrowBack } from 'react-icons/io';
+import { NavLink } from 'react-router-dom';
+import { motion } from 'framer-motion';
+
+const NavBar = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 5px 10px;
+
+  > div:nth-of-type(1) {
+    h3 {
+      margin: 10px 0;
+      color: #133B5C;
+    }
+  }
+  > div:nth-of-type(2) {
+    display: flex;
+
+    > span:nth-of-type(1) {
+      margin-right: 15px;
+    }
+  }
+`
+
+const containerVariants = {
+  hidden: {
+    y: -100
+  },
+  visible: {
+    y:0,
+    transition: {duration:0.5, stiffness:500}
+  }
+}
+
+const Header = ({home}) => {
+  return ( 
+    <motion.div variants={containerVariants} initial='hidden' animate='visible' >
+    <NavBar>
+      {!home && <NavLink to="/">
+        <IoIosArrowBack size='20px'  color="#133B5C" />
+      </NavLink>}
+      <div>
+        <h3>Stock Metrics</h3>
+      </div>
+      <div>
+        <span><BsFillMicFill size='18px'   color="#133B5C"/></span>
+        <span> <IoIosSettings size='18px'  color="#133B5C"/></span>
+      </div>
+    </NavBar>
+    </motion.div>
+   );
+}
+ 
+export default Header;
